43675a03e5dea58218e6450de66acd837bb6252b
kennedy7 <kennedyhillary6@gmail.com>
2024-03-13 22:44:28 +0100

    AuthController and authRouter

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import './db'
 import categoryRouter from './router/category'
+import authRoute from './router/authRoute'
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.static("src/public"));
 
 
 app.use('/categories', categoryRouter);
+app.use('/api/auth', authRoute);
 
 
 const PORT = 3002
